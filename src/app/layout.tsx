fd0ec56a6566ca369ce72a41a690ba55ac7f4463
JOE DAVID <joelsofflmail@gmail.com>
2026-02-04 16:39:42 +0000

    it still shows firebase icon as favicon..rectify it and replace with log

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: 'https://cxecpvkwmuxrzkqzpgwd.supabase.co/storage/v1/object/public/webp_bucket/Whisk_226d7aa74fcf94b9b3048a44a6dd1758dr.png',
+    shortcut: 'https://cxecpvkwmuxrzkqzpgwd.supabase.co/storage/v1/object/public/webp_bucket/Whisk_226d7aa74fcf94b9b3048a44a6dd1758dr.png',
     apple: 'https://cxecpvkwmuxrzkqzpgwd.supabase.co/storage/v1/object/public/webp_bucket/Whisk_226d7aa74fcf94b9b3048a44a6dd1758dr.png',
   },
   manifest: '/manifest.json',
